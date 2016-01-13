d02a85e83571a859c4ac3fd613935ab614266e9d
LauriKauhanen <H4215@student.jamk.fi>
2016-01-13 18:51:43 +0200

    Added Jinja2 templating

--- calm_temple_8514/index.py
+++ calm_temple_8514/index.py
@@ -1,5 +1,6 @@
 from calm_temple_8514 import app
+from flask import render_template
 
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index(name=None):
+    return render_template('hello.html', name=name)
