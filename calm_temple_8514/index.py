c72f8d52b22a5123a294fdf8d3a6e8925242fc76
LauriKauhanen <H4215@student.jamk.fi>
2016-01-13 18:57:50 +0200

    Jinja2

--- calm_temple_8514/index.py
+++ calm_temple_8514/index.py
@@ -2,5 +2,5 @@ from calm_temple_8514 import app
 from flask import render_template
 
 @app.route('/')
-def index(name=None):
+def index(name='World!'):
     return render_template('hello.html', name=name)
