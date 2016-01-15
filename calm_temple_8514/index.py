730de621955aa6b79f6a341679de7f84f58becd7
LauriKauhanen <H4215@student.jamk.fi>
2016-01-15 15:12:29 +0200

    Add Jinja2 templating

--- calm_temple_8514/index.py
+++ calm_temple_8514/index.py
@@ -1,5 +1,19 @@
 from calm_temple_8514 import app
+from flask import render_template, request, make_response,redirect, url_for
+from time import time
 
+#Index
 @app.route('/')
-def index():
-    return 'Hello World!'
+def index(username=None):
+    resp = make_response(render_template('index.html'))
+    resp.set_cookie('visit', str(time()))
+    return resp
+
+@app.route('/username', methods=['POST'])
+def username():
+    return NotImplementedError
+
+#404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
